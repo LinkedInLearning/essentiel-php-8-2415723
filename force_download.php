5dac8c5725045c733eaa1a0d5ef005f54280cab4
Julian Layen <layenjulian@gmail.com>
2021-07-29 22:50:43 +0200

    08_04_d

--- force_download.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$file = './files/data.csv';
-header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=\"" . basename($file) . "\""); 
-readfile($file); 
-
-?>
